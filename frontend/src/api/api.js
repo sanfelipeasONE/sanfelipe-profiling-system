0803bfdb06f259a180adf9c9e9ecdef915b1e9be
AgentLouie <louiezxc20@gmail.com>
2026-02-25 12:08:42 +0800

    changing baseurl

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("API BASE URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL // Your FastAPI URL
 });
